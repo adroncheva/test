241f31b8442abaaf91acf096352946d6cf3a8b2e
Alexandra Droncheva <alexdroncheva_98@abv.bg>
2022-01-21 11:00:50 +0200

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Montserrat', sans-serif;
             }
         </style>
     </head>
